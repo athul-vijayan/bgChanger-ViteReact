a0a1173a5457200efc264f6da038613533fc4d43
Athul Vijayan <web.wizard.athul@gmail.com>
2023-10-15 17:16:26 +0530

    bgcolor changing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,46 @@
+import { useState } from "react";
 import "./App.css";
 
 
 
 function App() {
+  
+  const [color, setColor]=useState('olive');
+
+  var mainContainer=document.getElementById('mainContainer');
+  if(mainContainer){
+    mainContainer.style.backgroundColor=color;
+  }
+
+  var bgColorChange=(newColor)=>{
+
+    setColor(newColor);
+    if(mainContainer){
+      mainContainer.style.backgroundColor=newColor;
+    } 
+    var buttons=document.querySelectorAll('button')
+    if(newColor=='black'){
+      buttons.forEach((button)=>{
+        console.log(button.style)
+        button.style.color='white'
+      })
+      
+    }
+  
+  }
+  console.log(color);
   return (
-  <>wizard</>
+  <div className="w-full h-screen duration-200" id="mainContainer">
+    <button className="m-4" onClick={()=>bgColorChange('red')}>RED</button>
+    <button className="m-4" onClick={()=>bgColorChange('black')}>black</button>
+    <button className="m-4" onClick={()=>bgColorChange('pink')}>pink</button>
+    <button className="m-4" onClick={()=>bgColorChange('violet')}>violet</button>
+    <button className="m-4" onClick={()=>bgColorChange('magenta')}>magenta</button>
+    <button className="m-4" onClick={()=>bgColorChange('indigo')}>indigo</button>
+
+
+  </div>
+  
   );
 }
 
