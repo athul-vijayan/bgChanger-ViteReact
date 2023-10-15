6b46e4d1354601007124cb7a5709197544735d0a
Athul Vijayan <web.wizard.athul@gmail.com>
2023-10-15 18:33:57 +0530

    design improved

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,81 @@
 import { useState } from "react";
 import "./App.css";
 
-
-
 function App() {
-  
-  const [color, setColor]=useState('olive');
+  var mainContainer = document.getElementById("mainContainer");
+  const [color, setColor] = useState("");
 
-  var mainContainer=document.getElementById('mainContainer');
-  if(mainContainer){
-    mainContainer.style.backgroundColor=color;
-  }
-
-  var bgColorChange=(newColor)=>{
-
+  var bgColorChange = (newColor) => {
     setColor(newColor);
-    if(mainContainer){
-      mainContainer.style.backgroundColor=newColor;
-    } 
-    var buttons=document.querySelectorAll('button')
-    if(newColor=='black'){
-      buttons.forEach((button)=>{
-        console.log(button.style)
-        button.style.color='white'
-      })
-      
+    if (mainContainer) {
+      mainContainer.style.backgroundColor = newColor;
     }
-  
-  }
+  };
+
+  var btns = document.querySelectorAll("button");
+  btns.forEach((btn) => {
+    btn.innerHTML = btn.innerHTML.toUpperCase();
+  });
+
   console.log(color);
   return (
-  <div className="w-full h-screen duration-200" id="mainContainer">
-    <button className="m-4" onClick={()=>bgColorChange('red')}>RED</button>
-    <button className="m-4" onClick={()=>bgColorChange('black')}>black</button>
-    <button className="m-4" onClick={()=>bgColorChange('pink')}>pink</button>
-    <button className="m-4" onClick={()=>bgColorChange('violet')}>violet</button>
-    <button className="m-4" onClick={()=>bgColorChange('magenta')}>magenta</button>
-    <button className="m-4" onClick={()=>bgColorChange('indigo')}>indigo</button>
-
-
-  </div>
-  
+    <div
+      className="w-full h-screen duration-200 text-center"
+      id="mainContainer"
+    >
+      <div className="justify-center m-auto">
+        <button
+          style={{ backgroundColor: "olive" }}
+          className="m-4 outline-none    text-white bg-violet-500  px-8 rounded-2xl p-4"
+          onClick={() => bgColorChange("olive")}
+        >
+          olive
+        </button>
+        <button
+          style={{ backgroundColor: "black" }}
+          className="m-4 px-8 rounded-2xl p-4 bg-violet-500 text-white"
+          onClick={() => bgColorChange("black")}
+        >
+          black
+        </button>
+        <button
+          style={{ backgroundColor: "pink" }}
+          className="m-4 px-8 rounded-2xl p-4 text-white bg-violet-500"
+          onClick={() => bgColorChange("pink")}
+        >
+          pink
+        </button>
+        <button
+          style={{ backgroundColor: "violet" }}
+          className="m-4 px-8 rounded-2xl p-4 text-white bg-violet-500"
+          onClick={() => bgColorChange("violet")}
+        >
+          violet
+        </button>
+        <button
+          style={{ backgroundColor: "magenta" }}
+          className="m-4 px-8 rounded-2xl p-4 text-white bg-violet-500"
+          onClick={() => bgColorChange("magenta")}
+        >
+          magenta
+        </button>
+        <button
+          style={{ backgroundColor: "indigo" }}
+          className="m-4 px-8 rounded-2xl p-4 text-white bg-violet-500"
+          onClick={() => bgColorChange("indigo")}
+        >
+          indigo
+        </button>
+        <button
+          style={{ backgroundColor: "lavender" }}
+          className="m-4 px-8 rounded-2xl p-4 text-white bg-violet-500"
+          onClick={() => bgColorChange("lavender")}
+        >
+          lavender
+        </button>
+      </div>
+    </div>
   );
 }
 
 export default App;
-
-
